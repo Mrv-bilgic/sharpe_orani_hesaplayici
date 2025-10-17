818609dc18fcf2c3f5ef0981de2cd4485cfc5f83
Mrv-bilgic <mrvblgc@hotmail.com>
2025-10-17 16:16:10 +0300

    Initial Commit

---
+++ algoritma2508/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  algoritma2508
+//
+//  Created by Merve Bilgiç on 17.10.2025.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
